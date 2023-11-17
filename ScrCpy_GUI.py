8aea41c34b14fb84a0895047e5fe60acfb1e73bd
imvickykumar999 <imvickykumar999@gmail.com>
2023-11-17 18:55:39 +0530

    ls -p /sdcard

--- ScrCpy_GUI.py
+++ ScrCpy_GUI.py
@@ -157,11 +157,36 @@ if __name__ == "__main__":
     t2.join()
 
 '''
->>> adb shell ls -R /sdcard/Download/Telegram/static/
+# https://stackoverflow.com/a/21725893/11493297
+Option  Description
 
-/sdcard/Download/Telegram/static:
-    Gabbar Bhi Nachega (feat. Nidhi Sharma)69.m4a.mp3
-    example.apk
-    image_2023-11-08_16-33-55.png
-    vix.png
+-a  Displays all files.
+-b  Displays nonprinting characters in octal.
+-c  Displays files by file timestamp.
+-C  Displays files in a columnar format (default)
+-d  Displays only directories.
+-f  Interprets each name as a directory, not a file.
+-F  Flags filenames.
+-g  Displays the long format listing, but exclude the owner name.
+-i  Displays the inode for each file.
+-l  Displays the long format listing.
+-L  Displays the file or directory referenced by a symbolic link.
+-m  Displays the names as a comma-separated list.
+-n  Displays the long format listing, with GID and UID numbers.
+-o  Displays the long format listing, but excludes group name.
+-p  Displays directories with /
+-q  Displays all nonprinting characters as ?
+-r  Displays files in reverse order.
+-R  Displays subdirectories as well.
+-t  Displays newest files first. (based on timestamp)
+-u  Displays files by the file access time.
+-x  Displays files as rows across the screen.
+-1  Displays each entry on a line.
+
+>>> adb shell ls -p /sdcard/Download/Telegram
+
+    Archive/
+    Reels.apk
+    screenshot.png
+    static/
 '''
